0364e37605d9e939bd5e904eedf33da90f95acde
Maja <majaclaraschewe@gmail.com>
2026-09-14 17:01:29 +0200

    try to solve 3.6, fail a bit

--- Assignment_2/Expr/Expr.fs
+++ Assignment_2/Expr/Expr.fs
@@ -6,6 +6,7 @@ module Expr
 
 open System.IO
 open Absyn
+open Parse
 
 (* From file expr/expr.sml * Simple arithmetic expressions *)
  
@@ -333,3 +334,9 @@ let s3 = scomp e3 []
 let intsToFile (inss : int list) (fname : string) = 
     let text = String.concat " " (List.map string inss)
     System.IO.File.WriteAllText(fname, text);;
+
+(* 3.6: compString : string -> sinstr list that parses a string as an
+   expression and compiles it to stack machine code. *)
+
+let compString (sexpr : string) =
+    fromString sexpr |> scomp;;
--- Assignment_3/Expr/obj/Debug/net10.0/parse.AssemblyInfo.fs
+++ Assignment_3/Expr/obj/Debug/net10.0/parse.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("parse")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+160e9689854a4c35da24f67a31c87dca3c812315")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5e66137558d13544cd885eab9e3723297714b85")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("parse")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("parse")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
